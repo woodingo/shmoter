9f41cfd9d186be00dd132ff79d36a4d2663b751b
andrew.yudin <andrew.yudin@nordclan.com>
2019-11-25 22:33:16 +0400

    all camera buttons

--- components/Camera.js
+++ components/Camera.js
@@ -2,7 +2,9 @@ import React, { useState, useRef, useEffect } from 'react';
 import { Platform } from 'react-native';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Camera as RNCamera } from 'expo-camera';
+import FeatherIcon from '@expo/vector-icons/Feather';
 import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
 
 const updateRatio = (set, ref) => async () => {
   if (Platform.OS === 'android' && ref.current.getSupportedRatiosAsync) {
@@ -10,6 +12,22 @@ const updateRatio = (set, ref) => async () => {
     const [actualRatio] = ratios;
     set(actualRatio);
   }
+};
+
+const snap = ref => async () => {
+  if (ref) {
+    let photo = await ref.current.takePictureAsync();
+    console.log(photo);
+  }
+};
+
+const pickImage = async () => {
+  let result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.All,
+    quality: 1,
+  });
+
+  console.log(result);
 };
 
 const Camera = props => {
@@ -40,8 +58,17 @@ const Camera = props => {
       ref={cameraRef}
     >
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={styles.flipButton} onPress={onTypeChange}>
-          <Text style={styles.flipButtonText}>Flip</Text>
+        <TouchableOpacity style={styles.button} onPress={pickImage}>
+          <FeatherIcon name="image" style={styles.icon}></FeatherIcon>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.createPhoto} onPress={snap(cameraRef)}>
+          <FeatherIcon
+            name="search"
+            style={styles.createPhotoIcon}
+          ></FeatherIcon>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={onTypeChange}>
+          <FeatherIcon name="refresh-cw" style={styles.icon}></FeatherIcon>
         </TouchableOpacity>
       </View>
     </RNCamera>
@@ -62,19 +89,40 @@ const styles = StyleSheet.create({
   },
   buttonsContainer: {
     flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-  },
-  flipButton: {
-    flex: 0.1,
     alignSelf: 'flex-end',
     alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
   },
-  flipButtonText: {
-    fontSize: 18,
-    marginBottom: 10,
-    marginLeft: 10,
+  button: {
+    height: 48,
+    width: 48,
+    marginVertical: 16,
+    marginHorizontal: 8,
+    borderRadius: 32,
+    borderWidth: 1,
+    borderColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  createPhoto: {
+    height: 64,
+    width: 64,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 24,
+    marginVertical: 16,
+    backgroundColor: 'white',
+    borderRadius: 32,
+    borderWidth: 8,
+    borderColor: 'rgba(255, 255, 255, 0.6)',
+  },
+  createPhotoIcon: {
+    fontSize: 24,
+  },
+  icon: {
     color: 'white',
+    fontSize: 24,
   },
   noPermissions: {
     flex: 1,
--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from '../screens/HomeScreen';
 
-import MainTabNavigator from './MainTabNavigator';
+// import MainTabNavigator from './MainTabNavigator';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    // You could add another route here for authentication.
-    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator,
-  })
-);
+// export default createAppContainer(
+//   createSwitchNavigator({
+//     // You could add another route here for authentication.
+//     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+//     Main: MainTabNavigator,
+//   })
+// );
+
+const routes = {
+  Home: HomeScreen,
+};
+
+const AppStack = createStackNavigator(routes);
+export default createAppContainer(AppStack);
